135a63b4745f575beb156b92751799231deb9d9e
Roman Cheplyaka <roma@ro-che.info>
2017-03-26 00:25:01 +0200

    Rewrite with classes

--- conn_comps.cc
+++ conn_comps.cc
@@ -3,25 +3,103 @@
 #include <cstring>
 #include <cstdint>
 #include <algorithm>
+#include <stdexcept>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/connected_components.hpp>
 
 using namespace std;
-using namespace boost;
 
-typedef adjacency_list<vecS, vecS, undirectedS> Graph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
 
-Graph construct_graph(samFile *bam, bam_hdr_t *header) {
+class BamRecord {
+  using deleter = void(*)(bam1_t*);
+  private:
+    unique_ptr<bam1_t, deleter> unq;
+  public:
+    bool eof;
+    BamRecord() : unq(bam_init1(), bam_destroy1) {};
+    bam1_t* ptr() {
+      return unq.get();
+    }
+    string qname() {
+      return string(bam_get_qname(unq.get()));
+    }
+    int32_t ref_id() {
+      return unq.get()->core.tid;
+    }
+};
+
+class BamHeader {
+  using deleter = void(*)(bam_hdr_t*);
+  private:
+    shared_ptr<bam_hdr_t> sptr;
+  public:
+    BamHeader() {};
+    BamHeader(bam_hdr_t *header) : sptr(header, bam_hdr_destroy) {};
+    bam_hdr_t* ptr() {
+      return sptr.get();
+    }
+    bool const operator!(){
+      return !sptr;
+    }
+};
+
+class BamFile {
+  using deleter = int(*)(htsFile*);
+  private:
+    unique_ptr<samFile, deleter> unq{nullptr, hts_close};
+    BamHeader header;
+    void read_header() {
+      if (!header) {
+        bam_hdr_t *hdr = sam_hdr_read(unq.get());
+        if (!hdr) {
+          throw runtime_error("Could not read header");
+        }
+        header = BamHeader(hdr);
+      }
+    }
+  public:
+    BamFile(const char *path) {
+      samFile *file = sam_open(path, "r");
+      if (!file) {
+        stringstream msg;
+        msg << "Could not open input file: " << strerror(errno);
+        throw runtime_error(msg.str());
+      }
+      unq = unique_ptr<samFile, deleter>(file, hts_close);
+    };
+    samFile* ptr() {
+      return unq.get();
+    }
+    BamHeader get_header() {
+      read_header();
+      return header;
+    }
+    BamRecord next_record() {
+      BamRecord rec;
+
+      // ensure that the header is read
+      read_header();
+
+      int r = sam_read1(unq.get(), header.ptr(), rec.ptr());
+      if (r == -1)
+        rec.eof = true;
+      if (r < -1)
+        throw runtime_error("The file appears corrupt");
+      return rec;
+    }
+};
+
+Graph construct_graph(BamFile file) {
   string last_qname;
   int last_isoform;
   bool first = true;
   Graph g;
 
-  bam1_t *b = bam_init1();
-
-  while (sam_read1(bam, header, b) >= 0) {
-    string this_qname = bam_get_qname(b);
-    int this_isoform = b->core.tid;
+  BamRecord rec;
+  while (rec = file.next_record(), !rec.eof) {
+    string this_qname = rec.qname();
+    int this_isoform = rec.ref_id();
     if (!first && last_qname == this_qname)
       add_edge(last_isoform, this_isoform, g);
 
@@ -30,7 +108,6 @@ Graph construct_graph(samFile *bam, bam_hdr_t *header) {
     first = false;
   }
 
-  bam_destroy1(b);
   return g;
 }
 
@@ -40,26 +117,11 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  samFile *bam = sam_open(argv[1], "r");
-  if (!bam) {
-    cerr << "Could not open input file: " << strerror(errno) << endl;
-    exit(1);
-  }
-
-  bam_hdr_t *header = sam_hdr_read(bam);
-  if (!bam) {
-    cerr << "Could not read header" << endl;
-    exit(1);
-  }
-
   // TODO check sorting order
-  Graph g = construct_graph(bam, header);
-
-  sam_close(bam);
+  Graph g = construct_graph(BamFile(argv[1]));
 
   vector<int> component(num_vertices(g));
   int num = connected_components(g, &component[0]);
-  // Calculate max comp size
   map<int,int> count;
   for (int i : component) {
     count[i]++;
